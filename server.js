1618d44abff6e87fd3cced56aad2b0243ac17027
Gervais-MIHINDOU <69824421+Gervais-MIHINDOU@users.noreply.github.com>
2022-01-09 17:36:39 +0100

    update

--- server.js
+++ server.js
@@ -3,13 +3,5 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/moneysHome'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/moneysHome/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
