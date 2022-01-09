2725241086f1971f466593036aac7e1336ca0861
Gervais-MIHINDOU <69824421+Gervais-MIHINDOU@users.noreply.github.com>
2022-01-09 18:29:43 +0100

    delete file

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
