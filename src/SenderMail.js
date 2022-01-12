1932c29797ea9c09dff96c3ddc5e76483e97b0d9
Lysiah <lysiahmengue@gmail.com>
2022-01-12 12:56:03 -0500

    correctif demamde par iris

--- src/SenderMail.js
+++ src/SenderMail.js
@@ -6,12 +6,12 @@ console.log(process.env.YOUR_SERVICE_ID);
 const SenderEmail = (email,message,user_name) => {
 
     init(process.env.YOUR_USER_ID);
-    var form = new FormData();
+    var form ={}; 
     form.append('email',email)
     form.append('message',message)
     form.append('user_name',user_name)
 
-    emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form, process.env.YOUR_USER_ID)
+    emailjs.send(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form, process.env.YOUR_USER_ID)
       .then((result) => {
           console.log(result.text);
       }, (error) => {
