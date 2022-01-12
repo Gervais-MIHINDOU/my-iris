0ef16298c66e2ad670915dd45c2aad2c64996650
Lysiah <lysiahmengue@gmail.com>
2022-01-12 12:13:57 -0500

    correctif demamde par iris

--- src/SenderMail.js
+++ src/SenderMail.js
@@ -1,9 +1,11 @@
 import emailjs from '@emailjs/browser';
+import{ init } from '@emailjs/browser';
 require('dotenv').config()
 console.log(process.env.YOUR_SERVICE_ID);
 
 const SenderEmail = (email,message,user_name) => {
 
+    init(process.env.YOUR_USER_ID);
     var form = new FormData();
     form.append('email',email)
     form.append('message',message)
